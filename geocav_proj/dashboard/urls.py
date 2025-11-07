db8b12e61bbd921e962d0a22bf639c7b2cb9f947
ShrutiPatil13 <50633145+ShrutiPatil13@users.noreply.github.com>
2025-11-07 15:07:27 -0600

    add network analysis page

--- geocav_proj/dashboard/urls.py
+++ geocav_proj/dashboard/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard_view, name='dashboard_view'),
+    path('', views.home, name='home'),
+    path('geospatial/', views.dashboard_view, name='dashboard_view'),
+    #path('geospatial/', views.geospatial_dashboard, name='geospatial_dashboard'),
+    path('network/', views.network_analysis, name='network_analysis'),
+    path('api/network/<slug:cancer>.json', views.network_json_by_slug, name='network_json_by_slug'),
+    path('molecular/', views.molecular_analysis, name='molecular_analysis'),
     # path('choropleth', views.choropleth, name='choropleth'),
     # path('pie', views.pie, name='pie'),
     # path('dotDensity', views.dotDensity, name='dotDensity'),
