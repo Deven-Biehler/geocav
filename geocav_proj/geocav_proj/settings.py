2f513824435caa66cdd89c95b61db3bd95ddfc5e
Deven-Biehler <deven.biehler@wsu.edu>
2026-02-19 11:34:19 -0800

    production ready settings.py

--- geocav_proj/geocav_proj/settings.py
+++ geocav_proj/geocav_proj/settings.py
@@ -20,8 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "Test"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+if not SECRET_KEY:
+    raise RuntimeError("DJANGO_SECRET_KEY is not set")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
